41745926bee126a7a3949ad6bae007f6c6f123b7
Tridib Banik <tridib.perfect@gmail.com>
2025-05-26 14:29:50 -0400

    Fixed orders of execution

--- fortune.sh
+++ fortune.sh
@@ -2,19 +2,22 @@
 # Program to tell a persons fortune
 echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
-N=$(( $RANDOM%6 ))
-GET_FORTUNE() {
-  echo "Ask a yes or no question:"
-  read QUESTION
+N=$(( RANDOM % 6 ))
+
+GET_FORTUNE(){
   if [[ ! $1 ]]
-  then 
-    echo ${RESPONSES[$N]}
+  then
+    echo Ask a yes or no question:
   else
-    echo "Try again. Make sure it ends with a question mark:"
+    echo Try again. Make sure it ends with a question mark:
   fi
+  read QUESTION
 }
+
 GET_FORTUNE
+
 until [[ $QUESTION =~ \?$ ]]
-do  
+do
   GET_FORTUNE again
 done
+echo -e "\n${RESPONSES[$N]}"
